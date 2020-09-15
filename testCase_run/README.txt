0b0f7993a422760894dd0820a431d100173c3944
xy <785815707@qq.com>
2020-09-15 11:05:21 +0800

    v1.0

--- testCase_run/README.txt
+++
@@ -1,4 +0,0 @@
-﻿testCase.bat:运行用例
-testRepo.bat:生成报告
-
-请运行main生成以上两个文件
--- testCase_run/testRepo.bat
+++
@@ -1,3 +0,0 @@
-airtest report "G:\git\testCase\01_add_address.air" --log_root G:\git\testCase_report\01_add_address --outfile G:\git\testCase_report\01_add_address\log.html --lang zh
-airtest report "G:\git\testCase\02_add_machine.air" --log_root G:\git\testCase_report\02_add_machine --outfile G:\git\testCase_report\02_add_machine\log.html --lang zh
-airtest report "G:\git\testCase\03_del_address.air" --log_root G:\git\testCase_report\03_del_address --outfile G:\git\testCase_report\03_del_address\log.html --lang zh
